3ea754c4ceaab1fdffaec5d15a3b85690571ff0c
Hans <hanstrax@gmail.com>
2013-10-19 14:11:36 -0300

    remove details js effect

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 //var api_host = "http://outro.vitenow.com";
-var api_host = "http://192.168.0.178:3000";
+//var api_host = "http://192.168.0.178:3000";
+//var api_host = "192.168.1.113:3000"
 //var api_host = "http://192.168.1.101:3000";
 //var api_host = "http://162.243.16.96";
-//var api_host = "localhost:3000"
+var api_host = "http://localhost:3000"
 
 
 //Models
